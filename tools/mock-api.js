ea5dfbfb84b55f740a2d3e18de560b72577d94f0
Robert K. Bell <robert.k.bell@gmail.com>
2017-03-21 17:31:20 +1100

    feat: switch from `_page` to `_offset`, improve error handling

--- tools/mock-api.js
+++ tools/mock-api.js
@@ -1,4 +1,5 @@
-const log = require('../lib/logs')({name: 'mock-api'});
+const {relevantProps, createLogger} = require('../lib/logs');
+const log = createLogger({name: 'mock-api'});
 log.info('▶ starting');
 
 const appRoot = require('app-root-path');
@@ -10,18 +11,23 @@ const fs = Promise.promisifyAll(require("fs"));
 const {
   bimap,
   chain,
-  curry2,
+  curry3,
   curry4,
-  fromMaybe,
   get,
   gets,
   is,
   Left,
   map,
   maybeToEither,
+  max,
   pipe,
   Right,
 } = require('sanctuary');
+
+const defaultPageSize = 10;
+const maxPageSize = 50;
+
+
 
 
 /*
@@ -41,7 +47,7 @@ const mocksfileₚₑ =
     .then(Right)
     .catch(error => {
       let s = 'error reading mocks file';
-      log.error({readFileError: error}, s);
+      log.error({readFileError: relevantProps(error)}, s);
       return Left({message: s, error});
     });
 
@@ -52,19 +58,19 @@ const router = new Router({
 
 
 /** wrapResponse :: a -> Object a
-*
-* Emulate the wrapping that Accelo provides.
-*
-* (there's a better way to write that type signature, right?)
-*/
+ *
+ * Emulate the wrapping that Accelo provides.
+ *
+ * (there's a better way to write that type signature, right?)
+ */
 function wrapResponse(data) {
   return {
-    "meta" : {
-      "version" : "0.1.1",
-      "status" : "ok",
-      "message" : "Everything executed as expected."
+    "meta": {
+      "version": "0.1.1",
+      "status": "ok",
+      "message": "Everything executed as expected."
     },
-    "response" : data
+    "response": data
   }
 }
 
@@ -83,16 +89,15 @@ function getMock(path, mocksₑ) {
   )
 }
 
-/** getPage :: Integer -> Array (Array a) -> Array a
+/** getPage :: Integer -> Integer -> Array a -> Array a
  */
-const getPage = curry2(function _getPage(page, mocks) {
-  const got = get(is(Array), `${page}`, mocks);
-  return fromMaybe([], got);
+const getPage = curry3(function _getPage(limit, offset, mocks) {
+  return mocks.slice(offset, offset + limit);
 });
 
 /** addSimpleEndpoint :: Promise (Either Object Object) -> Router -> String -> Array String -> router
  *
- * Create a simple mock of a paginated Accelo endpoint.
+ * Create a simple mock of a "paginated" Accelo endpoint.
  *
  * todo: make that type sig more informative
  */
@@ -100,30 +105,41 @@ const addSimpleEndpoint = curry4(function _addSimpleEndpoint(mocksfileₚₑ, ro
   log.trace({path}, 'registering simple endpoint');
   return router.get(path,
     async(ctx, next) => {
-      const mockₑ = getMock(mockpath, await mocksfileₚₑ);
-      const page = ctx.request.query.page - 1 || 0;
-      const pageDataₑ = map(getPage(page), mockₑ);
+      // NB: there's nothing to handle exceptions from this function...
+      try {
+        const mockₑ = getMock(mockpath, await mocksfileₚₑ);
+        const limit = max(
+          fromMaybe(
+            defaultPageSize,
+            parseInt(10, ctx.request.query._limit)
+          ),
+          maxPageSize
+        );
+        const offset = fromMaybe(
+          0,
+          parseInt(ctx.request.query._offset)
+        );
+        const pageDataₑ = map(getPage(limit, offset), mockₑ);
 
-      function sendBody(data) {
-        try {
+        function sendBody(data) {
           ctx.body = wrapResponse(data);
           next();
-        } catch (error) {
-          log.warn({error, url: ctx.url}, '500 error while responding');
-          ctx.body = {message: error.message};
-          ctx.status = error.status || 500
         }
-      }
 
-      function sendNothing(msg) {
-        const s = 'nonexistent mock data';
-        log.warn({url: ctx.url, msg: msg.message, mockpath}, s);
-        ctx.status = 500;
-        ctx.body = {message: s, response: msg};
-        next();
-      }
+        function sendNothing(msg) {
+          const s = 'nonexistent mock data';
+          log.warn({url: ctx.url, msg: msg.message, mockpath}, s);
+          ctx.status = 500;
+          ctx.body = {message: s, response: msg};
+          next();
+        }
 
-      bimap(sendNothing, sendBody, pageDataₑ);
+        bimap(sendNothing, sendBody, pageDataₑ);
+      } catch (error) {
+        log.error({error: relevantProps(error), url: ctx.url}, 'exception while responding');
+        ctx.body = {message: error.message};
+        ctx.status = error.status || 500
+      }
     }
   );
 });
